c97ac378308bbdcd8f791dd0f1b41b047ba0b8c5
luo4neck <luo4neck@gmail.com>
2014-12-01 21:51:26 +0000

    test

--- Backup.py
+++ Backup.py
@@ -1,32 +1,43 @@
 #! /usr/python
 
 import os
-# import git
+import git
 import tarfile
 import time
 
-target_name = './Blog' + os.sep + time.strftime('%Y%m%d') + '.tar.gz'
-
+### backup for blog ####
+target_name = './Blog' + os.sep + 'blog_backup.tar.gz'
 tar = tarfile.open(target_name, 'w:gz')
-
+for root,dirs,files in os.walk('/home/neck/Blog/source/img'):
+	for names in files:
+		fullpath = os.path.join(root,names)
+		tar.add(fullpath,arcname=names)
 for root,dirs,files in os.walk('/home/neck/Blog/source/_posts'):
 	for names in files:
 		fullpath = os.path.join(root,names)
 		tar.add(fullpath,arcname=names)
-
 tar.list()
 tar.close()
+### backup for blog ####
+
+
 
 git_add = ('git add *')
 git_commit = ('git commit -m %s') % time.strftime('%Y%m%d')
 git_push = ('git push')
-git_commit = ('git commit -m %s') % time.strftime('%Y%m%d')
 
-if os.system( git_add ) != 0:
-	print 'git add was wrong'
+repo = git.Repo('.')
+print repo.git.add('*')
+print repo.git.commit(m='test')
+# print repo.git.commit(m='%s') % time.strftime('%Y%m%d')
 
-if os.system( git_commit ) != 0:
-	print 'git commit was wrong'
+# git_commit = ('git commit -m %s') % time.strftime('%Y%m%d')
 
-if os.system( git_push ) != 0:
-	print 'git push was wrong'
+# if os.system( git_add ) != 0:
+#print 'git add was wrong'
+
+# if os.system( git_commit ) != 0:
+# print 'git commit was wrong'
+
+# if os.system( git_push ) != 0:
+# print 'git push was wrong'
